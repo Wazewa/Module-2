3b956f56b744833d8702866b2d546ac12850c817
Korolev Ivan <wazewa73@gmail.com>
2026-06-23 16:13:14 +0400

    feat: add solutions for Task1 and Task2

---
+++ src/main/java/academy/mediasoft/team/Task1.java
@@ -0,0 +1,54 @@
+package academy.mediasoft.team;
+
+import java.util.Random;
+
+public class Task1 {
+    static int[] createdArrayOfProductionYear(int count) {
+        Random rnd = new Random();
+
+        int[] arr = new int[count];
+
+        int minYear = 2000;
+        int maxYear = 2025;
+
+        for(int i = 0; i < count; i++) {
+            arr[i] = rnd.nextInt(minYear, maxYear);
+        }
+
+        return arr;
+    }
+
+    static void printCarsAfterYear(int yearOfProduction, int[] arr) {
+        for(int year: arr) {
+            if(year > yearOfProduction) {
+                System.out.println(year);
+            }
+        }
+    }
+
+    static void getAverageAge(int count, int[] arr) {
+        double averageAge = 0.0;
+
+        int currentYear = 2026;
+
+        for(int model: arr) {
+            averageAge += currentYear - model;
+        }
+        averageAge /= count;
+
+        System.out.println("Средний возраст равен: " + averageAge);
+    }
+
+    public static void main(String[] args) {
+        int count = 50;
+
+        int[] arr = createdArrayOfProductionYear(count);
+
+        int yearOfProduction = 2015;
+
+        printCarsAfterYear(yearOfProduction, arr);
+
+        getAverageAge(count, arr);
+
+    }
+}
---
+++ src/main/java/academy/mediasoft/team/Task2.java
@@ -0,0 +1,75 @@
+package academy.mediasoft.team;
+
+import java.util.*;
+
+public class Task2 {
+    static List<String> createListModel() {
+        List<String> list = new ArrayList<>();
+
+        list.add("Toyota RAV4");
+        list.add("BMW X5");
+        list.add("Peugeot 207");
+        list.add("Lada Priora");
+        list.add("VAZ 2110");
+        list.add("Peugeot 207");
+        list.add("Tesla Model S");
+        list.add("Ford Sierra");
+        list.add("VAZ 2110");
+
+        return list;
+    }
+
+    static List<String> removingDuplicates(List<String> list) {
+        List<String> uniqueModel = new ArrayList<>();
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for(String model: list) {
+            if(!map.containsKey(model)) {
+                map.put(model, 1);
+                uniqueModel.add(model);
+            }
+        }
+
+        return uniqueModel;
+    }
+
+    static void sortedModel(List<String> list) {
+        list.sort(Comparator.reverseOrder());
+    }
+
+    static Set<String> convertToSet(List<String> list) {
+        return new HashSet<>(list);
+    }
+
+    static void replaceTesla(Set<String> set) {
+        List<String> teslaList = new ArrayList<>();
+
+        for(String model: set) {
+            if (model.contains("Tesla")) {
+                teslaList.add(model);
+            }
+        }
+
+        for(String model: teslaList) {
+            set.remove(model);
+            set.add("ELECTRO_CAR");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        List<String> nameModel = createListModel();
+
+        nameModel = removingDuplicates(nameModel);
+
+        sortedModel(nameModel);
+
+        nameModel.forEach(System.out::println);
+
+        Set<String> setModel = convertToSet(nameModel);
+
+        replaceTesla(setModel);
+
+    }
+}
